3af4ecb73e359de4f41792d71ca383430735bc0f
oliver-007 <oliverahmed000@gmail.com>
2023-05-30 13:55:37 +0600

    reservation functionality added

--- app/listings/[listingId]/page.jsx
+++ app/listings/[listingId]/page.jsx
@@ -3,9 +3,11 @@ import getListingById from "@/app/actions/getListingById";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 const ListingPage = async ({ params }) => {
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
@@ -19,7 +21,11 @@ const ListingPage = async ({ params }) => {
   return (
     <div>
       <ClientOnly>
-        <ListingClient listing={listing} currentUser={currentUser} />
+        <ListingClient
+          listing={listing}
+          currentUser={currentUser}
+          reservations={reservations}
+        />
       </ClientOnly>
     </div>
   );
